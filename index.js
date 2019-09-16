8e0a30e264a028262a921306f7588d0889485e83
Sinclair Seo <sinclairseo@gmail.com>
2019-09-16 11:06:03 +0900

    #2.4 Handling Routes with Express

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
 
-const PORT = 5000;
+const PORT = 4000;
 
 function handlelistening() {
     console.log(`Listening on: http://localhost:${PORT}`);
 }
+
+function handlehome(req, res) {
+    //console.log(req);
+    res.send('Welcome WeTube Home');
+}
+
+function handleprofile(req, res){
+    res.send("You're on my profile now.");
+}
+
+app.get("/", handlehome);
+app.get("/profile", handleprofile);
 
 app.listen(PORT, handlelistening);
