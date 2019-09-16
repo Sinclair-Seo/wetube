b224c48cab2e59332470cae5dfeb37cc410f50b8
Sinclair Seo <sinclairseo@gmail.com>
2019-09-16 11:38:35 +0900

    #2.5 ES6 on NodeJS using Babel

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require('express');
+//const express = require('express');
+import express from "express";
 const app = express();
 
 const PORT = 4000;
 
+/*
 function handlelistening() {
     console.log(`Listening on: http://localhost:${PORT}`);
 }
@@ -15,8 +17,12 @@ function handlehome(req, res) {
 function handleprofile(req, res){
     res.send("You're on my profile now.");
 }
+*/
 
-app.get("/", handlehome);
-app.get("/profile", handleprofile);
+const handlelistening = () => console.log(`Listening on: http://localhost:${PORT}`);
+const handlehome = (req, res) => res.send("Welcome WeTube Home!");
+const handleprofile = (req, res) => res.send("You're on my profile now.");
 
 app.listen(PORT, handlelistening);
+app.get("/", handlehome);
+app.get("/profile", handleprofile);
