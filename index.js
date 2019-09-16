76086453ad17f7f996c6abf14a09e9e46ec39394
Sinclair Seo <sinclairseo@gmail.com>
2019-09-16 10:48:35 +0900

    #2.3 Your Frist Express Server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+const express = require('express');
+const app = express();
+
+const PORT = 5000;
+
+function handlelistening() {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handlelistening);
