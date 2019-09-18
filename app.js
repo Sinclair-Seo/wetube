5d692e7971bf6e806607286e34f1f3d4d7410889
Sinclair Seo <sinclairseo@gmail.com>
2019-09-18 12:47:18 +0900

    #2.8 Express Core: Routing

--- app.js
+++ app.js
@@ -4,26 +4,24 @@ import logger from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Welcome WeTube Home!");
 
 const handleProfile = (req, res) => res.send("You're on my profile now.");
 
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(logger("dev"));
 
 app.get("/", handleHome);
 
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(helmet());
-app.use(logger("combined"));
-
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;
