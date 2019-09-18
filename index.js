6f21bacfa4d1b30a4071550ca211d8fe476b9e87
Sinclair Seo <sinclairseo@gmail.com>
2019-09-18 11:05:49 +0900

    #2.7 Express Core: Middlewares part Two

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
 //const express = require('express');
 import express from "express";
+import logger from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
-
-/*
-function handlelistening() {
-    console.log(`Listening on: http://localhost:${PORT}`);
-}
-
-function handlehome(req, res) {
-    //console.log(req);
-    res.send('Welcome WeTube Home');
-}
-
-function handleprofile(req, res){
-    res.send("You're on my profile now.");
-}
-*/
 
 const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 
@@ -25,14 +15,14 @@ const handleHome = (req, res) => res.send("Welcome WeTube Home!");
 
 const handleProfile = (req, res) => res.send("You're on my profile now.");
 
-const betweenHome = (req, res, next) => {
-    console.log("Between");
-    next();
-}
 
 app.get("/", handleHome);
 
-app.use(betweenHome);
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(logger("combined"));
 
 app.get("/profile", handleProfile);
 
