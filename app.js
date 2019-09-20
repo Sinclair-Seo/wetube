f97e752661bcdac80fc1151eed2d000d730629f3
Sinclair Seo <sinclairseo@gmail.com>
2019-09-20 10:54:26 +0900

    #2.9 MVC Pattern part Two

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-//const express = require('express');
 import express from "express";
 import logger from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
-
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 const app = express();
-
-const handleHome = (req, res) => res.send("Welcome WeTube Home!");
-
-const handleProfile = (req, res) => res.send("You're on my profile now.");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -18,10 +14,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(logger("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 export default app;
