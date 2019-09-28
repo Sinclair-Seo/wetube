f2b5b23de49e08c63974436ca1bd24c7613bc699
Sinclair Seo <sinclairseo@gmail.com>
2019-09-28 11:52:07 +0900

    #2.18 Search Controller

--- controllers/globalController.js
+++ controllers/globalController.js
@@ -2,4 +2,10 @@ export const contHome = (req, res) => res.render("home", { pageTitle: "Home" });
 export const contJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const contLogin = (req, res) => res.render("login", { pageTitle: "Login" });
 export const contLogout = (req, res) => res.render("logout", { pageTitle: "Logout" });
-export const contSearch = (req, res) => res.render("search", { pageTitle: "Search" });
+export const contSearch = (req, res) => {
+    //console.log(req.query);
+    const {
+        query: { term: searchingBy }
+    } = req;
+    res.render("search", { pageTitle: "Search", searchingBy });
+}
