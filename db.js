5e6c7e394c0f406278b53f050a126fa9a0aed37d
Sinclair Seo <sinclairseo@gmail.com>
2019-09-29 16:25:27 +0900

    #3.1 Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,3 +1,21 @@
+import mongoose from "mongoose";
+
+mongoose.connect(
+    "mongodb://localhost:27017/we-tube",
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false
+    }
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("️️️☑️  Connect to DB");
+const handleError = error => console.log(`❌  Error on DB Connection: ${error} `);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
+
 export const videos = [
     {
         id:392849,
